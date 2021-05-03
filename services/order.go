f3c10650a36fb1940ce63bfb282e03f7b294b546
wahyu hadi <wahyuhadi@192.168.2.4>
2021-05-04 05:50:24 +0800

    Fix: double order gaps

--- services/order.go
+++ services/order.go
@@ -36,10 +36,10 @@ func GetOrdersDetails(pair string) {
 	if TOTALBUY > TOTALSELL {
 		logrus.Info("Order Gaps       : ", ac.FormatMoney(TOTALBUY-TOTALSELL))
 		logrus.Info("Gain  Gaps       : ", gaps)
+	} else {
+		logrus.Warn("Order Gaps       : ", ac.FormatMoney(TOTALBUY-TOTALSELL))
+		logrus.Warn("Gain  Gaps       : ", TOTALBUY/TOTALSELL)
 	}
-	logrus.Warn("Order Gaps       : ", ac.FormatMoney(TOTALBUY-TOTALSELL))
-	logrus.Warn("Gain  Gaps       : ", TOTALBUY/TOTALSELL)
-
 	if gaps >= 5 {
 		logrus.Info("Gain Severity    : Very Strong market")
 	}
